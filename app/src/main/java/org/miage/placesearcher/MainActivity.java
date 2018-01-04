f9262774c9d41b450c9e565784abdbabedc0d96c
Alex Morel <alex.morel@irealite.com>
2018-01-04 11:08:01 +0100

    [Android] Exercice 1 - Add rating bar to default activity and increment its value each time activity is shown

--- app/src/main/java/org/miage/placesearcher/MainActivity.java
+++ app/src/main/java/org/miage/placesearcher/MainActivity.java
@@ -1,13 +1,28 @@
 package org.miage.placesearcher;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RatingBar mRatingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the rating bar from layout file
+        this.mRatingBar = (RatingBar) this.findViewById(R.id.ratingBar);
+    }
+
+    @Override
+    protected void onResume() {
+        // Do NOT forget to call super.onResume()
+        super.onResume();
+
+        // Increment rating bar's rating
+        this.mRatingBar.setRating(this.mRatingBar.getRating() + 1);
     }
 }

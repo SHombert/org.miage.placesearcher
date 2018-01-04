040b694606e076191735b80f817eb0e7517b434d
Alex Morel <alex.morel@irealite.com>
2018-01-04 17:26:01 +0100

    [Android] Exercice 8 - Step 2 : play mp3 sound on item clicks

--- app/src/main/java/org/miage/placesearcher/MainActivity.java
+++ app/src/main/java/org/miage/placesearcher/MainActivity.java
@@ -1,12 +1,17 @@
 package org.miage.placesearcher;
 
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import org.miage.placesearcher.model.Place;
 import org.miage.placesearcher.ui.PlaceAdapter;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,6 +39,24 @@ public class MainActivity extends AppCompatActivity {
         // Instanciance PlaceAdapter
         PlaceAdapter placeAdapter = new PlaceAdapter(this, places);
         mListView.setAdapter(placeAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                AssetFileDescriptor afd = null;
+                try {
+                    afd = getAssets().openFd("house.mp3");
+
+                    MediaPlayer player = new MediaPlayer();
+                    player.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+                    player.prepare();
+                    player.start();
+
+                } catch (IOException e) {
+                    // Silent catch : sound will not be played
+                }
+            }
+        });
     }
 
     @Override
